87a0291625096455c272934a2c11ca59db612ffe
EC2 Default User <ec2-user@ip-172-31-22-83.ap-northeast-1.compute.internal>
2022-12-20 07:56:13 +0000

    [Fix]

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,6 @@ class Report < ApplicationRecord
   belongs_to :reporter, class_name: "User"
   belongs_to :reported, class_name: "User"
 
-  validates :reason, presence:true
+  #validates :reason, presence:true
 
 end
